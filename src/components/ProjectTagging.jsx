daba475594c914c532be1dc2e419add4cd5ce1dc
hiriorima <g2117001@fun.ac.jp>
2017-11-21 14:41:57 +0900

    Use react-player

--- src/components/ProjectTagging.jsx
+++ src/components/ProjectTagging.jsx
@@ -4,36 +4,102 @@ import Debug from 'debug';
 
 import { connect } from 'react-redux';
 
-import Player from './Player';
-import ChartView from'../chart/ChartView';
+import ReactPlayer from 'react-player'
+import ChartView from '../chart/ChartView';
 
 class ProjectTagging extends React.Component {
 
     constructor(props) {
         super(props);
+
+        this.state = {
+            url: null,
+            playing: false,
+            volume: 0.8,
+            muted: false,
+            played: 0,
+        }
+
+        this.playPause = () => {
+            this.setState({ playing: !this.state.playing })
+        }
+        this.onPlay = () => {
+            this.setState({ playing: true })
+        }
+        this.onPause = () => {
+            this.setState({ playing: false })
+        }
+        this.setVolume = e => {
+            this.setState({ volume: parseFloat(e.target.value) })
+        }
+        this.toggleMuted = () => {
+            this.setState({ muted: !this.state.muted })
+        }
+        this.onSeekChange = e => {
+            this.setState({ played: parseFloat(e.target.value) })
+            this.refs.player.seekTo(parseFloat(e.target.value))
+        }
+        this.onProgress = state => {
+            this.setState(state)
+        }
     };
 
-        render() {
-            return (
-                <div className="taggingproject">
-                    <center>
-                    <Player />
-                    <svg id="chart_left" ref="chart_left"></svg>
-                    <svg id="chart_right" ref="chart_right"></svg>
-                    </center>
-                </div>
-            );
-        }
+    render() {
+        return (
+            <div className="taggingproject">
+                <center>
+                    {this.props.project !== null ?
+                        <ReactPlayer
+                            ref={"player"}
+                            className='react-player'
+                            width='50%'
+                            height='50%'
+                            url={this.props.project.content[0].figure.file.file.url}
+                            playing={this.state.playing}
+                            volume={this.state.volume}
+                            muted={this.state.muted}
+                            onReady={() => console.log('onReady')}
+                            onStart={() => console.log('onStart')}
+                            onPlay={this.onPlay}
+                            onPause={this.onPause}
+                            onBuffer={() => console.log('onBuffer')}
+                            onSeek={e => console.log('onSeek', e)}
+                            onEnded={() => console.log('End')}
+                            onError={e => console.log('onError', e)}
+                            onProgress={this.onProgress}
+                        />
+                        :
+                        null
+                    }
+                    <div>
+                        <button onClick={this.playPause}>{this.state.playing ? 'Pause' : 'Play'}</button>
+                        <input
+                            type='range' min={0} max={1} step='any'
+                            value={this.state.played}
+                            onChange={this.onSeekChange}
+                        />
+                        <input type='range' min={0} max={1} step='any' value={this.state.volume} onChange={this.setVolume} />
+                        <input type='checkbox' checked={this.state.muted} onChange={this.toggleMuted} /> Muted
+                    </div>
+                    <div>
+                        <svg id="chart_left" ref="chart_left"></svg>
+                        <svg id="chart_right" ref="chart_right"></svg>
+                    </div>
+                </center>
+            </div>
+        );
+    }
 
-        componentWillUpdate(nextProps){
-            if(nextProps.project.sensor_infos[0].data.data.url.indexOf("left") >= 0){
-                ChartView.init(this.refs.chart_left, nextProps.project.sensor_infos[0].data.data.url);
-                ChartView.init(this.refs.chart_right, nextProps.project.sensor_infos[1].data.data.url);
-            }else {
-                ChartView.init(this.refs.chart_left, nextProps.project.sensor_infos[1].data.data.url);
-                ChartView.init(this.refs.chart_right, nextProps.project.sensor_infos[0].data.data.url);
-            }
+    componentWillUpdate(nextProps) {
+
+        if (nextProps.project.sensor_infos[0].data.data.url.indexOf("left") >= 0) {
+            ChartView.init(this.refs.chart_left, nextProps.project.sensor_infos[0].data.data.url);
+            ChartView.init(this.refs.chart_right, nextProps.project.sensor_infos[1].data.data.url);
+        } else {
+            ChartView.init(this.refs.chart_left, nextProps.project.sensor_infos[1].data.data.url);
+            ChartView.init(this.refs.chart_right, nextProps.project.sensor_infos[0].data.data.url);
         }
+    }
 }
 
 ProjectTagging.propTypes = {
@@ -42,7 +108,7 @@ ProjectTagging.propTypes = {
 
 const mapStateToProps = (state) => (
     {
-        project: state.manager.targetProject  
+        project: state.manager.targetProject
     }
 );
 
@@ -52,3 +118,4 @@ const mapDispatchToProps = (dispatch) => (
 );
 
 export default connect(mapStateToProps, mapDispatchToProps)(ProjectTagging);
+
