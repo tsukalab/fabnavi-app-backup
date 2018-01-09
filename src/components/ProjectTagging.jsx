5041814c3bbae6bd4bab49c86b2665a9175eb43f
hiriorima <g2117001@fun.ac.jp>
2018-01-09 17:18:26 +0900

    Add video player

--- src/components/ProjectTagging.jsx
+++ src/components/ProjectTagging.jsx
@@ -4,17 +4,122 @@ import Debug from 'debug';
 
 import { connect } from 'react-redux';
 
+import ReactPlayer from 'react-player'
+import Duration from '../utils/Duration';
+
 class ProjectTagging extends React.Component {
 
     constructor(props) {
         super(props);
+
+        this.state = {
+            url: null,
+            playing: false,
+            volume: 0.8,
+            muted: false,
+            played: 0,
+            duration: 0,
+        }
+
+        this.playPause = () => {
+            this.setState({ playing: !this.state.playing })
+        }
+        this.onPlay = () => {
+            this.setState({ playing: true })
+        }
+        this.onPause = () => {
+            this.setState({ playing: false })
+        }
+        this.setVolume = e => {
+            this.setState({ volume: parseFloat(e.target.value) })
+        }
+        this.toggleMuted = () => {
+            this.setState({ muted: !this.state.muted })
+        }
+        this.onSeekChange = e => {
+            this.setState({ played: parseFloat(e.target.value) })
+            this.refs.player.seekTo(parseFloat(e.target.value))
+        }
+        this.onProgress = state => {
+            this.setState(state)
+        }
     };
 
-        render() {
-            return (
-                <div>ProjectTagging</div>
-            );
-        }
+    render() {
+        return (
+            <div className="taggingproject">
+                <style jsx>{`
+                    .input-range[type="range"] {
+                        -webkit-appearance: none;
+                        appearance: none;
+                        background-color: #c7c7c7;
+                        height: 2px;
+                        width: 400px;
+                        margin-left: 20px;
+                    }
+                    .play-button {
+                        width: 50px;
+                        height: 30px;
+                    }
+                    .input-range::-webkit-slider-thumb {
+                            -webkit-appearance: none;
+                            appearance: none;
+                            cursor: pointer;
+                            position: relative;
+                            border: none;
+                            width: 12px;
+                            height: 12px;
+                            display: block;
+                            background-color: #262626;
+                            border-radius: 50%;
+                            -webkit-border-radius: 50%;
+                          }
+                    .elapsed-time{
+                        margin-left: 20px;
+                    }
+                `}</style>
+                <center>
+                    {this.props.project !== null ?
+                        <ReactPlayer
+                            ref={"player"}
+                            className='react-player'
+                            width='50%'
+                            height='50%'
+                            url={this.props.project.content[0].figure.file.file.url}
+                            playing={this.state.playing}
+                            volume={this.state.volume}
+                            muted={this.state.muted}
+                            onReady={() => console.log('onReady')}
+                            onStart={() => console.log('onStart')}
+                            onPlay={this.onPlay}
+                            onPause={this.onPause}
+                            onBuffer={() => console.log('onBuffer')}
+                            onSeek={e => console.log('onSeek', e)}
+                            onEnded={() => console.log('End')}
+                            onError={e => console.log('onError', e)}
+                            onProgress={this.onProgress}
+                            onDuration={duration => this.setState({ duration })}
+                        />
+                        :
+                        null
+                    }
+                    <div>
+                        <button className="play-button" onClick={this.playPause}>{this.state.playing ? 'Pause' : 'Play'}</button>
+                        <input className="input-range"
+                            type='range' min={0} max={1} step='any'
+                            value={this.state.played}
+                            onChange={this.onSeekChange}
+                        />
+                        <Duration className="elapsed-time"
+                            seconds={this.state.duration * this.state.played}
+                        />
+                        <label>/</label>
+                        <Duration seconds={this.state.duration} />
+                    </div>
+                </center>
+            </div>
+        );
+    }
 }
 
 ProjectTagging.propTypes = {
@@ -23,7 +128,7 @@ ProjectTagging.propTypes = {
 
 const mapStateToProps = (state) => (
     {
-        project: state.manager.targetProject  
+        project: state.manager.targetProject
     }
 );
 
